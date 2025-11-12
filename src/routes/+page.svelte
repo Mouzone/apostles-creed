ee54cb0fcb016ac12b3f217f46202b6ad25d9bec
Mouzone <sunnyliu010@gmail.com>
2025-11-12 16:28:16 -0500

    Add apostles creed text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div>
+	<p>I believe in God,</p>
+	<p>the Father almighty,</p>
+	<p>Creator of heaven and Earth,</p>
+	<p>and in Jesus Christ, his only Son, our Lord,</p>
+	<p>who was conceived by the Holy Spirit,</p>
+	<p>born of the Virgin Mary,</p>
+	<p>suffered under Pontius Pilate,</p>
+	<p>was crucified, died and was buried;</p>
+	<p>he descended into hell;</p>
+	<p>on the third day he rose again from the dead;</p>
+	<p>he ascended into heaven,</p>
+	<p>and is seated at the right hand of God the Father almighty;</p>
+	<p>from there he will come to judge the living and the dead.</p>
+
+	<br />
+
+	<p>I believe in the Holy Spirit,</p>
+	<p>the holy catholic Church,</p>
+	<p>the communion of saints,</p>
+	<p>the forgiveness of sins,</p>
+	<p>the resurrection of the body,</p>
+	<p>and life everlasting.</p>
+
+	<br />
+	<p>Amen.</p>
+</div>
