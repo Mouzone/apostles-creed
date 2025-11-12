f5a1bd6743a252a70bf72bbdc5c1eab229648ef7
Mouzone <56100404+Mouzone@users.noreply.github.com>
2025-11-12 16:17:02 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Contents
+
+## Standard
+- svletekit + svelte5
+
+## Libraries
+- Normalize.css
+- @vite/pwa-assets-generator
+
+## Boilerplate Code
+- service-worker.ts
+- +layout.svelte file
+  - added pwa code
+  - imported normalize.css
+- manifest.json
+- pwa-assets.config.ts
+  - remove padding when creating apple icons
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import "normalize.css";
+	import { onMount } from "svelte";
+
+	async function detectSWUpdate() {
+		const registration = await navigator.serviceWorker.ready;
+
+		registration.addEventListener("updatefound", () => {
+			const newSW = registration.installing;
+			newSW?.addEventListener("statechange", () => {
+				if (newSW.state === "installed") {
+					if (confirm("New update available! Reload to update?")) {
+						newSW.postMessage({ type: "SKIP_WAITING" });
+						window.location.reload();
+					}
+				}
+			});
+		});
+	}
+
+	onMount(() => detectSWUpdate());
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<title>New Project</title>
+</svelte:head>
+
+{@render children()}
