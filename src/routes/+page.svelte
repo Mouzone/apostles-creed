45b6052dc1be7380b8f773e10fe14fc2e900188c
Mouzone <sunnyliu010@gmail.com>
2025-11-12 16:42:40 -0500

    Add data-line attribute to allow selection of the line

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,33 @@
 <div>
-	<p>I believe in God,</p>
-	<p>the Father almighty,</p>
-	<p>Creator of heaven and Earth,</p>
-	<p>and in Jesus Christ, his only Son, our Lord,</p>
-	<p>who was conceived by the Holy Spirit,</p>
-	<p>born of the Virgin Mary,</p>
-	<p>suffered under Pontius Pilate,</p>
-	<p>was crucified, died and was buried;</p>
-	<p>he descended into hell;</p>
-	<p>on the third day he rose again from the dead;</p>
-	<p>he ascended into heaven,</p>
-	<p>and is seated at the right hand of God the Father almighty;</p>
-	<p>from there he will come to judge the living and the dead.</p>
+	<p data-line="1">I believe in God,</p>
+	<p data-line="2">the Father almighty,</p>
+	<p data-line="3">Creator of heaven and Earth,</p>
+	<p data-line="4">and in Jesus Christ, his only Son, our Lord,</p>
+	<p data-line="5">who was conceived by the Holy Spirit,</p>
+	<p data-line="6">born of the Virgin Mary,</p>
+	<p data-line="7">suffered under Pontius Pilate,</p>
+	<p data-line="8">was crucified, died and was buried;</p>
+	<p data-line="9">he descended into hell;</p>
+	<p data-line="10">on the third day he rose again from the dead;</p>
+	<p data-line="11">he ascended into heaven,</p>
+	<p data-line="12">
+		and is seated at the right hand of God the Father almighty;
+	</p>
+	<p data-line="13">
+		from there he will come to judge the living and the dead.
+	</p>
 
 	<br />
 
-	<p>I believe in the Holy Spirit,</p>
-	<p>the holy catholic Church,</p>
-	<p>the communion of saints,</p>
-	<p>the forgiveness of sins,</p>
-	<p>the resurrection of the body,</p>
-	<p>and life everlasting.</p>
+	<p data-line="14">I believe in the Holy Spirit,</p>
+	<p data-line="15">the holy catholic Church,</p>
+	<p data-line="16">the communion of saints,</p>
+	<p data-line="17">the forgiveness of sins,</p>
+	<p data-line="18">the resurrection of the body,</p>
+	<p data-line="19">and life everlasting.</p>
 
 	<br />
-	<p>Amen.</p>
+	<p data-line="20">Amen.</p>
 </div>
 
 <style>
