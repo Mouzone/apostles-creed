292fc445202c2d6f9ed12b16bc8ca5c530381d6e
Mouzone <sunnyliu010@gmail.com>
2025-11-13 16:16:32 -0500

    Turn the verses of apostles creed into json for easier editing and clean up page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,23 @@
-<div>
-	<p data-line="1">I believe in God,</p>
-	<p data-line="2">the Father almighty,</p>
-	<p data-line="3">Creator of heaven and Earth,</p>
-	<p data-line="4">and in Jesus Christ, his only Son, our Lord,</p>
-	<p data-line="5">who was conceived by the Holy Spirit,</p>
-	<p data-line="6">born of the Virgin Mary,</p>
-	<p data-line="7">suffered under Pontius Pilate,</p>
-	<p data-line="8">was crucified, died and was buried;</p>
-	<p data-line="9">he descended into hell;</p>
-	<p data-line="10">on the third day he rose again from the dead;</p>
-	<p data-line="11">he ascended into heaven,</p>
-	<p data-line="12">
-		and is seated at the right hand of God the Father almighty;
-	</p>
-	<p data-line="13">
-		from there he will come to judge the living and the dead.
-	</p>
+<script>
+	let mode = $state("all");
+	let verse = $state(1);
+	$effect(() => {
+		if (mode === "all") {
+		} else if (mode === "verse") {
+		}
+	});
+</script>
 
-	<br />
-
-	<p data-line="14">I believe in the Holy Spirit,</p>
-	<p data-line="15">the holy catholic Church,</p>
-	<p data-line="16">the communion of saints,</p>
-	<p data-line="17">the forgiveness of sins,</p>
-	<p data-line="18">the resurrection of the body,</p>
-	<p data-line="19">and life everlasting.</p>
-
-	<br />
-	<p data-line="20">Amen.</p>
+<div id="page">
+	<button onclick={() => (mode === "all" ? "verse" : "all")}>
+		Switch Mode
+	</button>
+	<div id="verses">
+		<p
+			class="verse"
+			data-line="1"
+		></p>
+	</div>
 </div>
 
 <style>
@@ -45,7 +35,7 @@
 		font-weight: 300 800;
 		font-style: normal;
 	}
-	div {
+	#page {
 		width: 100dvw;
 		padding: 2em 3em 2em 3em;
 
